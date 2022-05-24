e570e8ac46a4febcac5a5296ca2a485b244e977e
ronentgreat <ronenh53@gmail.com>
2022-05-24 20:20:51 +0000

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 // Components
 import Header from './components/Header';
+import Home from './components/Home';
 
 // Styles
 import {GlobalStyle} from './GlobalStyle';
@@ -10,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      Start here.
+      <Home />
       <GlobalStyle/>
     </div>
   );
