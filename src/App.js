7f288544e35b0ce852ad881e6c0caccec3e8c663
ronentgreat <ronenh53@gmail.com>
2022-05-24 13:26:41 +0000

    added global style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// Styles
+import {GlobalStyle} from './GlobalStyle';
+
 function App() {
   return (
     <div className="App">
---
+++ src/GlobalStyle.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root{
+    --maxWidth: 128px;
+    --white: #fff;
+    --lightGrey: #eee;
+    --medGrey: #353535;
+    --darkGrey: #1c1c1c;
+    --fontSuperBig: 2.5rem;
+    --fontBig: 1.5rem;
+    --fontMed: 1.2rem;
+    --fontSmall: 1rem;
+  }
+
+  * {
+    box-sizing: border-box;
+    font-family: 'Abel', sans-serig;
+  }
+
+  body{
+    margin: 0;
+    padding: 0;
+
+    h1{
+      font-size: 2rem;
+      font-weight: 600;
+      color: var(--white);
+    }
+
+    h3{
+      font-size: 1.1rem;
+      font-weight: 600;
+    }
+
+    p{
+      font-size: 1rem;
+      color: var(--white)
+    }
+  }
+
+
+
+`
