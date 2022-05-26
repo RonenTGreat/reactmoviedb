49087e9f60e754ac350de0c49650777b57119da9
ronentgreat <ronenh53@gmail.com>
2022-05-26 23:13:09 +0000

    created thumbnail for movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // Components
 import HeroImage from "../components/HeroImage";
 import Grid from "./Grid";
+import Thumb from "./Thumb";
 
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -31,7 +32,16 @@ const Home = () => {
       }
       <Grid header='Popular Movies'>
         {state.results.map(movie => (
-          <div key={movie.id}>{movie.title }</div>
+          <Thumb 
+          key={movie.id}
+          clickable
+          image={
+            movie.poster_path
+            ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+            : NoImage
+          }
+          movieId = {movie.id}
+          />
         ))}
       </Grid>
     </>
---
+++ src/components/Thumb/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Styles
+import { Image } from "./Thumb.styles";
+
+const Thumb = ({image, movieId, clickable}) => (
+  <div>
+    <Image src={image} alt='movie-thumb'/>
+  </div>
+)
+
+export default Thumb;
