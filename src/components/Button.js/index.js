71f41f6e9783289ffe66061cb5eb60edf15a2450
ronentgreat <ronenh53@gmail.com>
2022-05-31 14:33:47 +0000

    added button to home page

---
+++ src/components/Button.js/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// Styles
+import { Wrapper } from "./Button.styles";
+
+const Button = ({text, callback}) => (
+  <Wrapper type="button" onClick={callback}>
+  {text}
+  </Wrapper>
+);
+
+export default Button;
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
 import SearchBar from "./SearchBar";
+import Button from "./Button.js";
 
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
@@ -47,7 +48,10 @@ const Home = () => {
           />
         ))}
       </Grid>
-      <Spinner/>
+      { loading && <Spinner/>}
+      {state.page < state.total_pages && !loading && (
+        <Button text='Load More' />
+      )}
     </>
   );
 };
