bced3d79d07adc176a42e27c7720a811171e3a74
ronentgreat <ronenh53@gmail.com>
2022-06-03 23:10:11 +0000

    added movie info bar

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,28 +9,31 @@ import Grid from "./Grid";
 import Spinner from "./Spinner";
 import BreadCumb from "./BreadCumb";
 import MovieInfo from "./MovieInfo";
-
+import MovieInfoBar from "./MovieInfoBar";
 // Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
 
 // Image
-import NoImage from '../images/no_image.jpg';
-
-
+import NoImage from "../images/no_image.jpg";
 
 const Movie = () => {
-  const {movieId} = useParams();
-  const {state: movie, loading, error } = useMovieFetch(movieId);
+  const { movieId } = useParams();
+  const { state: movie, loading, error } = useMovieFetch(movieId);
 
-  if(loading) return <Spinner />
-  if(error) return <div>Something went wrong</div>
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong</div>;
 
-  return(
+  return (
     <>
       <BreadCumb movieTitle={movie.original_title} />
-      <MovieInfo movie={movie}/>
+      <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
     </>
-  )
+  );
 };
 
 export default Movie;
