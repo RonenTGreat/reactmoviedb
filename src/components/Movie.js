7ac1a97aee06cc3a6b364662672939de61442d31
ronentgreat <ronenh53@gmail.com>
2022-06-03 23:43:08 +0000

    added actor grid

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,6 +10,7 @@ import Spinner from "./Spinner";
 import BreadCumb from "./BreadCumb";
 import MovieInfo from "./MovieInfo";
 import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
 // Hook
 import { useMovieFetch } from "../hooks/useMovieFetch";
 
@@ -32,6 +33,20 @@ const Movie = () => {
         budget={movie.budget}
         revenue={movie.revenue}
       />
+      <Grid header='Actors'>
+        {movie.actors.map(actor => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
     </>
   );
 };
