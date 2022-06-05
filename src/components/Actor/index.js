3045900a417cdc08d4fc7c9309bf508f017d1ee3
ronentgreat <ronenh53@gmail.com>
2022-06-05 21:57:51 +0000

    added proptype to actor

--- src/components/Actor/index.js
+++ src/components/Actor/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 // Styles
 import { Wrapper, Image } from "./Actor.styles";
@@ -9,6 +10,12 @@ const Actor = ({name, character, imageUrl}) => (
     <h3>{name}</h3>
     <p>{character}</p>
   </Wrapper>
-)
+);
+
+Actor.propTypes = {
+  name: PropTypes.string,
+  character: PropTypes.string,
+  imageUrl: PropTypes.string
+}
 
 export default Actor;
