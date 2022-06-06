cf60c922c245391bb2a8b8c589cf043997bb2b8f
ronentgreat <ronenh53@gmail.com>
2022-06-06 19:53:13 +0000

    persisted data to sessionStorage

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -2,6 +2,8 @@ import { useState, useEffect, useRef } from "react";
 
 // API
 import API from "../API";
+// Helpers
+import { isPersistedState } from "../helpers";
 
 const initialState = {
   page: 0,
@@ -39,6 +41,15 @@ export const useHomeFetch = () => {
 
   // Initial and search
   useEffect(() => {
+  if(!searchTerm){
+    const sessionState = isPersistedState('homeState');
+
+    if(sessionState) {
+      setState(sessionState);
+      return;
+    }
+  }
+
     setState(initialState);
     fetchMovies(1, searchTerm);
   }, [searchTerm]);
@@ -49,7 +60,13 @@ export const useHomeFetch = () => {
 
     fetchMovies(state.page + 1, searchTerm);
     setIsLoadingMore(false);
-  }, [isLoadingMore, searchTerm, state.page])
+  }, [isLoadingMore, searchTerm, state.page]);
+
+  // Write to sessionStorage
+  useEffect(() => {
+  if(!searchTerm) sessionStorage.setItem('homeState', JSON.stringify(state))
+
+  }, [searchTerm, state])
 
   return {state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore};
 }
